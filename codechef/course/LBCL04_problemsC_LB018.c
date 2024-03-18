bdb46a77cce96271b36afed360f74807c7161aca
Somnath-2024-40 <somnathsingpatar123@gmail.com>
2024-03-19 00:48:47 +0530

    my code

---
+++ codechef/course/LBCL04_problemsC_LB018.c
@@ -0,0 +1,30 @@
+// Division
+// We have already done addition and multiplication inside print. Let's do subtraction and division as well once.
+
+// For your revision, here are all the operators.
+
+// Operator	Description	Example
+// +	Addition	a + b
+// -	Subtraction	a - b
+// *	Multiplication	a * b
+// /	Division	a / b
+// Task
+// Write a program which does the following
+
+// Print the result of subtraction of 9 and 2
+// Print the result of division of 30 by 5
+// Output both results on separate lines
+#include <stdio.h>
+
+int main() {
+	
+	printf("%d", 9-2  );
+	printf("\n%d", 30/5);
+	
+	
+	
+
+	return 0;
+	
+	
+}
