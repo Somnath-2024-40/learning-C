1df187639f8886a76b0ad9a4739bdf1e67d37510
Somnath-2024-40 <somnathsingpatar123@gmail.com>
2024-03-20 17:54:23 +0530

    if and else

---
+++ codechef/course/LBCL13_problems_CLB066.c
@@ -0,0 +1,45 @@
+// If Statement
+// The else statement is optional.
+
+// Here is an example
+
+// grade = 95;
+// if (grade >= 90) {
+//     printf("You got an A");
+// }
+
+// // Output
+// // You got an A
+// In the above program, if the condition is True, then the printf statement is executed.
+
+// Task
+// Write a program which does the following
+
+// Take input for two integer variables a & b
+// Output "Coding is Fun" to the console if a is greater than b.
+// Sample 1:
+// Input
+// Output
+// 25
+// 20
+// Coding is Fun
+// Sample 2:
+// Input
+// Output
+// 20
+// 20
+#include <stdio.h>
+
+int main() {
+    int grade;
+	// your code goes here
+	grade = 25;
+	if (grade >=20) {
+	    printf("Coding is Fun");
+	    
+	}
+	return 0;
+    
+}
+
+ 
